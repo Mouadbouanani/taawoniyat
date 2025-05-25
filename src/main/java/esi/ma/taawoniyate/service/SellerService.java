b68b5c7d997b52ded901a85ac8a5e3b65006115c
Soufyan-Lekhouaja <soufyan.lekhouaja@esi.ac.ma>
2025-05-25 14:55:15 +0100

    25-5 15:00

--- src/main/java/esi/ma/taawoniyate/service/SellerService.java
+++ src/main/java/esi/ma/taawoniyate/service/SellerService.java
@@ -11,8 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -66,5 +68,11 @@ public class SellerService {
 
     public boolean existsById(Integer id) {
         return sellerRepository.existsById(id);
+    }
+    public List<Product> getProductsByBusinessName(String businessName) {
+        List<Seller> sellers = findByBusinessName(businessName);
+        List<Product> products = new ArrayList<>();
+        sellers.forEach(seller -> {products.addAll(seller.getProducts());});
+        return products;
     }
 }
